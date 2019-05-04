d0e795383b4ab195b3c5a2c8de1ff2d73679f9de
Abhishek <abhishek@exotel.com>
2019-05-04 10:13:28 +0530

    Add test folder and tests for 1.1-is_unique_test.js

---
+++ problems/datastructures/arrays-and-strings/1.1-is_unique_test.js
@@ -0,0 +1,16 @@
+const assert = require('chai').assert
+const isEveryCharacterUnique = require('./1.1-is_unique').isEveryCharacterUniquefunction
+
+describe('Testing 1.1-is_unique', _ => {
+  it('Input "avonte" must return true', function () {
+    assert.isTrue(isEveryCharacterUnique('avonte'))
+  })
+
+  it('Input "Droid" must return true', function () {
+    assert.isTrue(isEveryCharacterUnique('Droid'))
+  })
+
+  it('Input "apple" must return false', function () {
+    assert.isFalse(isEveryCharacterUnique('apple'))
+  })
+})
