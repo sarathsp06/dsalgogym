3d486bb598afdca76546f840816d393a1c4cd883
Abhishek <20806640+Abhicoding@users.noreply.github.com>
2019-05-15 15:41:01 +0530

    Enchancement/hound config (#15)
    updated hound and eslint config
    
    Co-authored-by: Abhicoding <shinganeabhishek@gmail.com>

--- problems/datastructures/arrays-and-strings/1.1-is_unique_test.js
+++ problems/datastructures/arrays-and-strings/1.1-is_unique_test.js
@@ -1,16 +1,16 @@
-const assert = require('chai').assert
-const isEveryCharacterUnique = require('./1.1-is_unique').isEveryCharacterUniquefunction
+const assert = require('chai').assert;
+const isEveryCharacterUnique = require('./1.1-is_unique').isEveryCharacterUniquefunction;
 
 describe('Testing 1.1-is_unique', _ => {
   it('Input "avonte" must return true', function () {
-    assert.isTrue(isEveryCharacterUnique('avonte'))
-  })
+    assert.isTrue(isEveryCharacterUnique('avonte'));
+  });
 
   it('Input "Droid" must return true', function () {
-    assert.isTrue(isEveryCharacterUnique('Droid'))
-  })
+    assert.isTrue(isEveryCharacterUnique('Droid'));
+  });
 
   it('Input "apple" must return false', function () {
-    assert.isFalse(isEveryCharacterUnique('apple'))
-  })
-})
+    assert.isFalse(isEveryCharacterUnique('apple'));
+  });
+});
