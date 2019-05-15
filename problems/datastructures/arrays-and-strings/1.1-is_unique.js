7edeccfd95d92051d7f932ae580fa8cff9d1ccc6
Abhishek <abhishek@exotel.com>
2019-05-15 12:26:13 +0530

    test

---
+++ problems/datastructures/arrays-and-strings/1.1-is_unique.js
@@ -0,0 +1,15 @@
+/**
+* Question:
+* Implement an algorithm to determine if a string has all unique characters.  What if you can not use additional data structures.
+ */
+
+function isEveryCharacterUnique (string) {
+  var specialString = ''
+  for (var i of string) {
+    if (specialString.includes(i)) return false
+    specialString += i
+  }
+  return true
+}
+
+exports.isEveryCharacterUniquefunction = isEveryCharacterUnique
