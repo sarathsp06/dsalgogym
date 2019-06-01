d3d9f46f99beca18655faa65db996be9621996cb
Arjun Mahishi <arjun.mahishi@gmail.com>
2019-06-01 19:44:16 +0530

    added semicolons as suggested by the 'hound'

--- scripts/updateIndex.js
+++ scripts/updateIndex.js
@@ -7,32 +7,32 @@ var indexString = "# Index\n\n"
 const getIndent = (n) => {
 	indent = ""
 	for (var i=0;i<n;i++){
-		indent += INDENT_CHAR
+		indent += INDENT_CHAR;
 	}
-	return indent
-}
+	return indent;
+};
 
 const walk = (dir, level=0) => {
 	let files = fs.readdirSync(dir).filter(file => 
 		fs.statSync(dir + "/" + file).isDirectory()
-	)
+	);
 	files.forEach(file => {
-		let filePath = `${dir}/${file}`
-		let link = filePath.replace("./", "")
-		indexString += `${getIndent(level)}- [${file}](${link})\n`
-		walk(filePath, level+1)
-	})
-}
+		let filePath = `${dir}/${file}`;
+		let link = filePath.replace("./", "");
+		indexString += `${getIndent(level)}- [${file}](${link})\n`;
+		walk(filePath, level+1);
+	});
+};
 
 const build = () => {
-	walk("./problems")
+	walk("./problems");
 	fs.writeFile("./INDEX.md", indexString, (err) => {
 		if(err) {
-			console.log("couldn't write to INDEX.md. err:", err)
+			console.log("couldn't write to INDEX.md. err:", err);
 		}else {
-			console.log("INDEX.md successfully updated!")
+			console.log("INDEX.md successfully updated!");
 		}
-	})
-}
+	});
+};
 
-build()
+build();
