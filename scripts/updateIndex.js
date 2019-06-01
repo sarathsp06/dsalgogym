4fae87a14646b6238e1b45dacd7bd1c50a4918f6
Arjun Mahishi <arjun.mahishi@gmail.com>
2019-06-01 19:34:54 +0530

    added script to generate INDEX.md when a new problem is added

---
+++ scripts/updateIndex.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+
+const INDENT_CHAR = "\t"
+
+var indexString = "# Index\n\n"
+
+const getIndent = (n) => {
+	indent = ""
+	for (var i=0;i<n;i++){
+		indent += INDENT_CHAR
+	}
+	return indent
+}
+
+const walk = (dir, level=0) => {
+	let files = fs.readdirSync(dir).filter(file => 
+		fs.statSync(dir + "/" + file).isDirectory()
+	)
+	files.forEach(file => {
+		let filePath = `${dir}/${file}`
+		let link = filePath.replace("./", "")
+		indexString += `${getIndent(level)}- [${file}](${link})\n`
+		walk(filePath, level+1)
+	})
+}
+
+const build = () => {
+	walk("./problems")
+	fs.writeFile("./INDEX.md", indexString, (err) => {
+		if(err) {
+			console.log("couldn't write to INDEX.md. err:", err)
+		}else {
+			console.log("INDEX.md successfully updated!")
+		}
+	})
+}
+
+build()
