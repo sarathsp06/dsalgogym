db41640581d8f2bd44a3ecfa0d5e5f5caaeece76
Abhishek <abhishek@exotel.com>
2019-07-01 18:51:57 +0530

    [Add]: problems/datastructures/arrays-and-strings check permutation problem in js

---
+++ problems/datastructures/arrays-and-strings/1.2-check_permutation_test.js
@@ -0,0 +1,20 @@
+const assert = require('chai').assert
+const isPermutation = require('./1.2-check_permutation').isPermutation
+
+describe('Testing 1.2-check_permutation', _ => {
+    it('Input "a" and "" must return false', function () {
+        assert.isFalse(isPermutation('a', ''))
+    })
+
+    it ('Input "a" and "a" must return true', function (){
+        assert.isTrue(isPermutation('a', 'a'))
+    })
+
+    it ('Input "aabbbc" and "abc" must return false', function () {
+        assert.isFalse(isPermutation('aabbbc', 'abc'))
+    })
+
+    it ('Input "abc" and "bca" must return true', function (){
+        assert.isTrue(isPermutation('abc', 'bca'))
+    })
+})
