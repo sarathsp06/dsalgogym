38eef596615195d3dd7025d69026a7cd383404cf
Abhishek Shingane <abhishekshingane@Abhisheks-MBP.Dlink>
2020-03-09 23:38:58 +0530

    add: longest substring with non-repeating characters

---
+++ problems/other/longestsubstring/longestsubstring_test.go
@@ -0,0 +1,23 @@
+package longestsubstring
+
+import (
+	"testing"
+)
+
+func Test_longestsubstring(t *testing.T) {
+	tests := []struct {
+		args string
+		want int
+	}{
+		{"abcabcbb", 3},
+		{"bbbbb", 1},
+		{"pwwkew", 3},
+	}
+	for _, tt := range tests {
+		t.Run("testing longestsubstring", func(t *testing.T) {
+			if got := lengthOfLongestSubstring(tt.args); tt.want != got {
+				t.Errorf("lengthOfLongestSubstring() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
