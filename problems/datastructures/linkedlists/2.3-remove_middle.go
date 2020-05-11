fb0cc85f0fb1a1201ca4ab3773f31c5ee3fb49f3
Athul Suresh <athul@exotel.in>
2020-05-11 22:18:06 +0530

    [2.3] Add code & tests

---
+++ problems/datastructures/linkedlists/2.3-remove_middle.go
@@ -0,0 +1,13 @@
+package linkedlists
+
+func removeMiddle(input []int, target int) string {
+	var ll LinkedList
+	ll.Init(input)
+	for i := ll.Head; i != nil && i.Next != nil; i = i.Next {
+		if i.Value == target {
+			i.Value = i.Next.Value
+			i.Next = i.Next.Next
+		}
+	}
+	return ll.String()
+}
